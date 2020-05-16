bc7f4de468baeb0368f2000f728e38b8d4ac2d8c
Karol <kgalaszewski95@gmail.com>
2020-05-16 20:34:12 +0100

    added flask basic api

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'Hello, Flask!'
+
+
+@app.route("/super_simple")
+def super_simple():
+    return "Hello from the Planetary API"
+
+if __name__ == '__main__':
+    app.run()
